afe102be66a6fe84ef5072591f4649ecd29390ac
braiancalpar <braian.santos@calpar.com.br>
2026-09-25 13:05:28 -0300

    feat: add order page and navigation button to complete purchase in Cart component

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -1,12 +1,14 @@
 import { MdClose } from "react-icons/md";
 import { useCart } from "../../store/contexts/cart";
 import CartItem from "../CartItem";
+import { useHistory } from "react-router-dom";
 
 type CartProps = {
   onClose: () => void;
 };
 
 const Cart: React.FC<CartProps> = ({ onClose }) => {
+  const navigation = useHistory();
   const {
     state: { books, cartTotal },
   } = useCart();
@@ -44,6 +46,16 @@ const Cart: React.FC<CartProps> = ({ onClose }) => {
                           Total da compra:
                         </h2>
                         <h3 className="text-2xl font-bold">R${cartTotal}</h3>
+                      </div>
+                      <div className="mt-8">
+                        <button
+                          className="py-3 w-full bg-[#EB9B00] hover:opacity-80 rounded-md shadow-md"
+                          onClick={() => navigation.push("/order")}
+                        >
+                          <h3 className="text-white text-lg font-medium">
+                            Finalizar Compra
+                          </h3>
+                        </button>
                       </div>
                     </>
                   )}
